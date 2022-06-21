4d95b659c6167c57538f9460db3f39a3743181b4
navaneethan2503 <nickilsekar2000@gmail.com>
2022-06-21 20:52:25 +0530

    MenuItem Entity Added

--- truYumWebApplication/Startup.cs
+++ truYumWebApplication/Startup.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace truYumWebApplication
 {
@@ -24,7 +25,9 @@ namespace truYumWebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connString = Configuration.GetConnectionString("ConnectionStringObject");
             services.AddControllers();
+            services.AddDbContext<AppDBContext>(context => context.UseSqlServer(connString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
